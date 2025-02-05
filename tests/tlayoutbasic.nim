7db062b1f11a946062909200dd2231f572f2153f
Jaremy Creechley <creechley@gmail.com>
2025-02-05 06:32:06 -0700

    fix content track length to include content size (or child sizes)

--- tests/tlayoutbasic.nim
+++ tests/tlayoutbasic.nim
@@ -118,7 +118,6 @@ suite "Basic CSS Layout Tests":
     check child2.box.w == 150 # (25% of 400) + 50
 
   test "Content based constraints":
-    prettyPrintWriteMode = cmTerminal
     let parent = newTestNode("parent", 0, 0, 400, 300)
     let child = newTestNode("child", 0, 0, 100, 100)
     let grandchild = newTestNode("grandchild", 0, 0, 150, 80)
@@ -138,7 +137,6 @@ suite "Basic CSS Layout Tests":
     printLayout(parent, cmTerminal)
 
     check child.box.w >= grandchild.box.w # Should be at least as wide as content
-    prettyPrintWriteMode = cmNone
 
   test "Position constraints":
     let parent = newTestNode("parent", 0, 0, 400, 300)

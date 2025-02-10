d2a552ce4b5e28364e231b575a2db50c8cb4e53d
Jaremy Creechley <creechley@gmail.com>
2025-02-10 02:05:23 -0700

    updates

--- tests/tlayout.nim
+++ tests/tlayout.nim
@@ -27,6 +27,64 @@ type
 proc `box=`*[T](v: T, box: UiBox) = 
   v.box = box
 
+var imageFiles: seq[string]
+
+proc writeHtmlSummary() =
+  var md = ""
+  md.add &"<html>\n"
+  md.add &"  <head>\n"
+  md.add """
+  <style>
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+    th, td {
+      padding: 15px;
+    }
+  </style>
+  """
+  md.add &"    </head>\n"
+  md.add &"  </head>\n"
+  md.add &"  <body>\n"
+  for file in imageFiles:
+    let exfile = file.replace(".png", "-expected.png")
+    md.add &"    <h2>{file}</h2>\n"
+    md.add &"    <table style='border-spacing: 10px; border: 1px solid black;'>\n"
+    md.add &"      <tr>\n"
+    md.add &"        <th>{file}</th>\n"
+    md.add &"        <th>{exfile}</th>\n"
+    md.add &"      </tr>\n"
+    md.add &"      <tr>\n"
+    md.add &"        <td><img src='{file}'/></td>\n"
+    md.add &"        <td><img src='{exfile}'/></td>\n"
+    md.add &"      </tr>\n"
+    md.add &"    </table>\n"
+    md.add &"    <br>\n"
+    md.add &"    <br>\n"
+  md.add &"  </body>\n"
+  md.add &"</html>\n"
+  writeFile("tests/tlayout.html", md)
+
+proc saveImage(gridTemplate: GridTemplate, box: UiBox, nodes: seq[GridNode], prefix = "") =
+  # echo "grid template post: ", repr gridTemplate
+  # echo "grid template post: ", repr box
+  # ==== item a ====
+  let image = newImage(box.w.int, box.h.int)
+  image.fill(rgba(255, 255, 255, 255))
+
+  # images
+  let ctx = newContext(image)
+  ctx.fillStyle = rgba(0, 255, 0, 255)
+
+  for i in 0 ..< nodes.len():
+    ctx.fillStyle = rgba(0, 55, 244, 255).asColor().spin(-15.3*i.float)
+    ctx.fillRoundedRect(nodes[i].box.toRect(), 12.0)
+
+  let file = fmt"tlayout-{prefix}{gridTemplate.autoFlow}.png"
+  imageFiles.add(file)
+  image.writeFile("tests" / file)
+
 proc makeGrid1(gridTemplate: var GridTemplate, cnt: int = 6): (seq[GridNode], UiBox) =
   # grid-template-columns: [first] 40px [line2] 50px [line3] auto [col4-start] 50px [five] 40px [end];
   # parseGridTemplateColumns gridTemplate, 60'ux 60'ux 60'ux 60'ux 60'ux
@@ -65,27 +123,6 @@ proc makeGrid1(gridTemplate: var GridTemplate, cnt: int = 6): (seq[GridNode], Ui
 
   printGrid(gridTemplate)
 
-var imageFiles: seq[string]
-
-proc saveImage(gridTemplate: GridTemplate, box: UiBox, nodes: seq[GridNode], prefix = "") =
-  # echo "grid template post: ", repr gridTemplate
-  # echo "grid template post: ", repr box
-  # ==== item a ====
-  let image = newImage(box.w.int, box.h.int)
-  image.fill(rgba(255, 255, 255, 255))
-
-  # images
-  let ctx = newContext(image)
-  ctx.fillStyle = rgba(0, 255, 0, 255)
-
-  for i in 0 ..< nodes.len():
-    ctx.fillStyle = rgba(0, 55, 244, 255).asColor().spin(-15.3*i.float)
-    ctx.fillRoundedRect(nodes[i].box.toRect(), 12.0)
-
-  let file = fmt"tlayout-{prefix}{gridTemplate.autoFlow}.png"
-  imageFiles.add(file)
-  image.writeFile("tests" / file)
-
 suite "grids":
 
   test "compute layout with auto flow":
@@ -121,40 +158,5 @@ suite "grids":
     let (n2, b2) = makeGrid1(gt2)
     saveImage(gt2, b2, n2, "extra-")
 
-  var md = ""
-  md.add &"<html>\n"
-  md.add &"  <head>\n"
-  md.add """
-  <style>
-    table, th, td {
-      border: 1px solid black;
-      border-collapse: collapse;
-    }
-    th, td {
-      padding: 15px;
-    }
-  </style>
-  """
-  md.add &"    </head>\n"
-  md.add &"  </head>\n"
-  md.add &"  <body>\n"
-  for file in imageFiles:
-    let exfile = file.replace(".png", "-expected.png")
-    md.add &"    <h2>{file}</h2>\n"
-    md.add &"    <table style='border-spacing: 10px; border: 1px solid black;'>\n"
-    md.add &"      <tr>\n"
-    md.add &"        <th>{file}</th>\n"
-    md.add &"        <th>{exfile}</th>\n"
-    md.add &"      </tr>\n"
-    md.add &"      <tr>\n"
-    md.add &"        <td><img src='{file}'/></td>\n"
-    md.add &"        <td><img src='{exfile}'/></td>\n"
-    md.add &"      </tr>\n"
-    md.add &"    </table>\n"
-    md.add &"    <br>\n"
-    md.add &"    <br>\n"
-  md.add &"  </body>\n"
-  md.add &"</html>\n"
-  writeFile("tests/tlayout.html", md)
 
-
+  writeHtmlSummary()

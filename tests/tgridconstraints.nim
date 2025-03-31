2a303b488f80d512c424bfe72b8faadc20d0b56e
Jaremy Creechley <creechley@gmail.com>
2025-03-30 22:33:56 -0600

    rename computelayout to layout

--- tests/tgridconstraints.nim
+++ tests/tgridconstraints.nim
@@ -11,7 +11,7 @@ import cssgrid/layout
 import pretty
 import macros
 
-
+import commontestutils
 template checkUiFloats(af, bf, ln, ls) =
   if abs(af.float-bf.float) >= 1.0e-3:
     checkpoint(`ln` & ":\n\tCheck failed: " & `ls` & "\n\tfield: " & astToStr(af) & " " & " value was: " & $af & " expected: " & $bf)
@@ -78,33 +78,61 @@ suite "CSS Grid Content Sizing":
     check gt.lines[drow][0].width == 200.UiScalar  # 1fr
 
   test "mixed content sizing":
-    var gt = newGridTemplate(
-      columns = @[
-        initGridLine(csContentMin()),
-        initGridLine(csAuto()),
-        initGridLine(csFrac(1))
-      ]
-    )
-    
-    var computedSizes: array[GridDir, Table[int, ComputedTrackSize]] = [
-      dcol: {
-        0: ComputedTrackSize(content: 50.UiScalar),
-        1: ComputedTrackSize(content: 75.UiScalar),
-        2: ComputedTrackSize(content: 100.UiScalar)
-      }.toTable,
-      drow: initTable[int, ComputedTrackSize]()
+    # Create parent node with mixed column sizing
+    var parent = newTestNode( name = "parent")
+    parent.cxSize = [500'ux, 100'ux]
+
+    parent.gridTemplate = newGridTemplate()
+    parent.gridTemplate.justifyItems = CxCenter
+    parent.gridTemplate.alignItems = CxCenter
+
+    parent.gridTemplate.lines[dcol] = @[
+      initGridLine(csContentMin()),
+      initGridLine(csAuto()),
+      initGridLine(csFrac(1))
     ]
 
-    gt.computeTracks(uiBox(0, 0, 500, 100), computedSizes)
+    # Create children to establish content sizes
+    var child1 = newTestNode( name = "child1", parent = parent)
+    child1.gridItem = newGridItem()
+    child1.gridItem.column = 1 // 2
+    child1.gridItem.row = 1 // 2
+    child1.cxMin = [50'ux, 50'ux]
 
-    # TL; DR: when setting a grid template, such as using grid-template-columns,
-    # auto is "greedy" for left over space except when it's used with fr units.
-    # When auto is used with fr, the auto row(s)/column(s) takes up just
-    # enough space to fit its content and any remaining space is given to the
-    # row(s)/columns(s) defined with fr units.
-    check gt.lines[dcol][0].width == 50.UiScalar   # min-content
-    check gt.lines[dcol][1].width == 75.UiScalar   # auto
-    check gt.lines[dcol][2].width == 375.UiScalar  # remaining space
+    var child2 = newTestNode( name = "child2", parent = parent)
+    child2.gridItem = newGridItem()
+    child2.gridItem.column = 2 // 3
+    child2.gridItem.row = 1 // 2
+    child2.cxMin = [75'ux, 75'ux]
+
+    var child3 = newTestNode( name = "child3", parent = parent)
+    child3.gridItem = newGridItem()
+    child3.gridItem.column = 3 // 4
+    child3.gridItem.row = 1 // 2
+    child3.cxMin = [100'ux, 100'ux]
+    child3.cxSize = [cx"auto", 100'ux]
+
+    # Add children to parent
+    parent.children.add(child1)
+    parent.children.add(child2)
+    parent.children.add(child3)
+
+    # Enable debug output
+    setPrettyPrintMode(cmTerminal)
+    defer: setPrettyPrintMode(cmNone)
+
+    # Compute layout
+    computeLayout(parent)
+
+    # Verify column widths
+    check parent.gridTemplate.lines[dcol][0].width == 50.UiScalar   # min-content
+    check parent.gridTemplate.lines[dcol][1].width == 75.UiScalar   # auto
+    check parent.gridTemplate.lines[dcol][2].width == 375.UiScalar  # remaining space (fr)
+
+    # Verify children are positioned correctly
+    check child1.box.x == 0.UiScalar
+    check child2.box.x == 50.UiScalar
+    check child3.box.x == 125.UiScalar
 
   test "content sizing with gaps":
     var gt = newGridTemplate(

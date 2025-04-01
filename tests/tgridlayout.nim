6886319be01917a53118bc61271a2e361a4ae119
Jaremy Creechley <creechley@gmail.com>
2025-03-31 18:46:12 -0600

    fix tests - redo container content size - v3

--- tests/tgridlayout.nim
+++ tests/tgridlayout.nim
@@ -96,8 +96,8 @@ suite "Compute Layout Tests":
         text.cxMin = [40'ux, 20.00'ux]
         text.cxMax = [200'ux, 300.00'ux]
       
-      # prettyPrintWriteMode = cmTerminal
-      # defer: prettyPrintWriteMode = cmNone
+      prettyPrintWriteMode = cmTerminal
+      defer: prettyPrintWriteMode = cmNone
       computeLayout(parent)
       # printLayout(parent, cmTerminal)
 
